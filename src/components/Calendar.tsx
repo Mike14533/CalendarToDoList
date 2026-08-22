1c88b2bdfaccac51564326df995febb7e323acb4
Mike14533 <mseanmahony02@gmail.com>
2026-08-22 15:03:38 +0100

    check for if the date is today

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -13,16 +13,17 @@ function Calendar(){
         </div>
         <div>
             <h1>{weekdays}</h1>
-            {cells.map((item: number, id: number) => (
-                {let today = now.getDate()
+            {cells.map((item: number, id: number) => {
+                let today = now.getDate()
 
-                }
+                return(
                 <div>
                     {today ? <h1>today</h1> : null}
                     <h1 className = "today: blue">{item}</h1>
                     <button>add task</button>
                 </div>
-            ))}
+                
+            )})}
         </div>
             
         </>
--- src/utils/useCalendar.ts
+++ src/utils/useCalendar.ts
@@ -52,19 +52,9 @@ function useCalendar(){
     function goNext(){return new Date(thisYear, thisMonth + 1)}
     function goPrev(){return new Date(thisYear, thisMonth - 1)}
     
-    // console.log("get start: " + getStart())
+    
 
-    // let cells = []
 
-    // for(let i = 5; i > - 2; i--){
-    //     cells.push(getEnd().getDate() - i)
-    // }
-    // for(let i = 1; i < getEnd().getDate() + 2; i++){
-    //     cells.push(i)
-    // }
-    // for(let i = 1; i < 7; i++){
-    //     cells.push(i)
-    // }
     return {now, thisYear, thisMonth,  getStart}
 }
 
